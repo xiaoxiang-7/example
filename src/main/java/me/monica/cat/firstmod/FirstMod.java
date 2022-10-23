726033fd632c6cff8a7d149122b113ae17cd9434
xiaoxiang-7 <eric410637340@gmail.com>
2022-10-23 19:01:03 +0800

    Initial commit

---
+++ src/main/java/me/monica/cat/firstmod/FirstMod.java
@@ -0,0 +1,10 @@
+package me.monica.cat.firstmod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class FirstMod implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/me/monica/cat/firstmod/client/FirstModClient.java
@@ -0,0 +1,13 @@
+package me.monica.cat.firstmod.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class FirstModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
